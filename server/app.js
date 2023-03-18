d01e95dbed886e63d5a9814bc680fcacf12323e3
AKANKSHAY9 <84265568+AKANKSHAY9@users.noreply.github.com>
2023-03-18 12:09:10 +0530

    client js

--- server/app.js
+++ server/app.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
 const User = require('./model/user')
-const bcrypt = require('bcryptjs')
 const checkPass=require('./utils/pass')
 const connectDb=require('./config/db');
 require('dotenv').config();
