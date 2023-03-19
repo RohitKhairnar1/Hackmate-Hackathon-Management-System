37d947a6dbbeb5ac004d707b1ab2849bac8b14f7
AKANKSHAY9 <84265568+AKANKSHAY9@users.noreply.github.com>
2023-03-19 20:27:43 +0530

    new

--- server/model/feedback.js
+++ server/model/feedback.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const feedbackSchema= new mongoose.Schema(
 	{
 		name: { type: String, required: true},
-		email: { type: String, required: true, unique: true },
+		email: { type: String, required: true, },
 		feedback: { type: String, required: true },
         
 	},
